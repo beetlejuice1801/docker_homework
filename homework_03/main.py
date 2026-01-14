860084de93e7e206bb6e9639700f52a82f21e791
beetlejuice1801 <a@lacatedral.ru>
2026-01-14 18:58:35 +0300

    Контейнер падает

--- homework_03/main.py
+++ homework_03/main.py
@@ -11,6 +11,3 @@ async def ping():
         status_code=status.HTTP_200_OK,
     )
     return response
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
